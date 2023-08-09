3ca01b59e16eb7d2e4452dc32070a6ee5cdd8c91
Oleg <panasenko23o@mail.ru>
2023-08-09 21:01:49 +1000

    add poetry

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,7 +3,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([], 0, "test") == 3
 
 
 def test_slice():
